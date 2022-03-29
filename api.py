1f35dfbbeb2ab7cf393c695e49602a5d64e5db61
JvRodrigues01 <62180451+JvRodrigues01@users.noreply.github.com>
2022-03-29 20:51:48 -0300

    Update api.py

--- api.py
+++ api.py
@@ -10,6 +10,7 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
         global arrayRequests
         self.send_response(200)
         self.send_header("Content-Type","application/json")
+        self.send_header("Access-Control-Allow-Origin","*")
         self.end_headers()
         self.wfile.write(bytes(json.dumps(arrayRequests), "UTF-8"))
 
